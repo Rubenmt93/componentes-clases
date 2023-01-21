325827428652f71f3debaa925332d12bab6a4989
Ruben Montes <rubenmontes330@gmail.com>
2023-01-21 12:50:10 +0000

    Estado de componentes

--- src/App.js
+++ src/App.js
@@ -2,9 +2,19 @@
 import { Component } from "react";
 
 class App extends Component{
+  state={
+    valor:3
+  }
   render (){
+    console.log(this.state);
     return(
-    <p> Hola Mundo </p>
+      <div> 
+        <p>Hola Mundo</p>
+        <button className={`${this.state.valor}`} 
+                onClick={() => this.setState({valor:1})}>
+          Enviar  
+        </button> 
+      </div>
     )
   }
 
